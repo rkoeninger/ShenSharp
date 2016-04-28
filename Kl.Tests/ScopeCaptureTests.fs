cce1b4ef3c070b52ecb235fad60f91e13be17769
rkoeninger <rkoeninger@att.net>
2016-04-27 21:58:07 -0400

    Added a couple of tests for scope capture

--- Kl.Tests/ScopeCaptureTests.fs
+++ Kl.Tests/ScopeCaptureTests.fs
@@ -12,5 +12,13 @@ type ScopeCaptureTests() =
         assertEq (Int 1) (runIt "(let X 1 (let F (lambda Y X) (F 0)))")
 
     [<Test>]
+    member this.``lambda expressions should not have access to symbols outside lexical scope``() =
+        assertEq (Sym "Y") (runIt "(let F (lambda X Y) (let Y 3 (F 0)))")
+
+    [<Test>]
     member this.``freeze expressions should capture local variables``() =
         assertEq (Int 1) (runIt "(let X 1 (let F (freeze X) (F)))")
+        
+    [<Test>]
+    member this.``freeze expressions should not have access to symbols outside lexical scope``() =
+        assertEq (Sym "Y") (runIt "(let F (freeze Y) (let Y 3 (F)))")
