ea4f2afbfa6feb7b9379cbb3575d43fe5f7b2969
rkoeninger <rkoeninger@att.net>
2016-04-27 22:36:31 -0400

    Added a few tests concerning symbol evaluation

--- Kl.Tests/SymbolEvaluationTests.fs
+++ Kl.Tests/SymbolEvaluationTests.fs
@@ -9,37 +9,27 @@ open TestCommon
 type SymbolEvaluationTests() =
 
     [<Test>]
-    member this.DefunAndResolutionOfDefinedFunctions() =
-        let env = Values.newEnv()
-        runInEnv env "(defun not (X) (if X false true))" |> ignore
-        runInEnv env "(defun xor (L R) (or (and L (not R)) (and (not L) R)))" |> ignore
-        assertTrue(runInEnv env "(xor true false)")
+    member this.``symbols not starting with an uppercase letter and not at the head of an application are always idle``() =
+        assertEq (Sym "abc") (runIt "(let Id (lambda X X) abc)")
+        assertEq (Sym "if") (runIt "(let Id (lambda X X) if)")
+        assertEq (Sym "+") (runIt "(let Id (lambda X X) +)")
 
     [<Test>]
-    member this.SymbolResolution() =
-        let klIsSymbol _ args =
-            match args with
-            | [Sym _] -> Values.truev
-            | [_] -> Values.falsev
-            | _ -> Values.err "symbol? only takes 1 argument"
-
-        let klId _ args =
-            match args with
-            | [x] -> x
-            | _ -> Values.err "id only takes 1 argument"
-
-        let env = baseEnv()
-        env.Globals.Functions.["symbol?"] <- Values.primitivev "symbol?" 1 klIsSymbol
-        env.Globals.Functions.["id"] <- Values.primitivev "id" 1 klId
-        assertTrue(runInEnv env "(symbol? run)")
-        assertTrue(runInEnv env "(= run (id run))")
+    member this.``symbols starting with an uppercase letter not at the head of an application are idle if not in local scope``() =
+        assertEq (Sym "ABC") (runIt "(let Id (lambda X X) ABC)")
 
     [<Test>]
     member this.``result of interning a string is equal to symbol with name that is equal to that string``() =
         assertEq (Sym "hi") (runIt "(intern \"hi\")")
+
+    [<Test>]
+    member this.``interned symbols can contain any characters``() =
+        assertEq (Sym "@!#$") (runIt "(intern \"@!#$\")")
+        assertEq (Sym "(),[];{}") (runIt "(intern \"(),[];{}\")")
+        assertEq (Sym "   ") (runIt "(intern \"   \")") // space space space
         
     [<Test>]
-    member this.``idle symbols``() =
+    member this.``both symbols starting with or with-out an uppercase letter or non-letter can be idle``() =
         assertEq
             (Cons(Sym "A", Cons(Sym "-->", Cons(Sym "boolean", Empty))))
             (runIt "(cons A (cons --> (cons boolean ())))")
