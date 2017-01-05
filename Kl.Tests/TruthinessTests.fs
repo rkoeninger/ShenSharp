70dca582f63831e0178a47883b7fc8a7450e0f56
Robert Koeninger <rkoeninger@att.net>
2017-01-05 11:04:26 -0500

    Improved naming in Truthiness tests to indicate that and/or always return Bool
    Added a couple of tests for if/cond

--- Kl.Tests/TruthinessTests.fs
+++ Kl.Tests/TruthinessTests.fs
@@ -9,7 +9,7 @@ open TestCommon
 type TruthinessTests() =
 
     [<Test>]
-    member this.``and expression should evaluate conditionals truthy``() =
+    member this.``and expression should evaluate conditionals truthy and always return Bool``() =
         assertTrue(run "(and 1 1)")
         assertTrue(run """(and "hi" -8.34)""")
         assertFalse(run "(and () true)")
@@ -17,7 +17,7 @@ type TruthinessTests() =
         assertFalse(run "(and false ())")
 
     [<Test>]
-    member this.``or expression should evaluate conditionals truthy``() =
+    member this.``or expression should evaluate conditionals truthy and always return Bool``() =
         assertTrue(run "(or 1 false)")
         assertTrue(run """(or "hi" -8.34)""")
         assertTrue(run "(or () 5)")
@@ -27,7 +27,9 @@ type TruthinessTests() =
     [<Test>]
     member this.``if expression should evaluate conditional truthy``() =
         assertEq (Int 2) (run "(if () 1 2)")
+        assertEq (Int 1) (run """(if "hi" 1 2)""")
 
     [<Test>]
     member this.``cond expression should evaluate conditional truthy``() =
         assertEq (Int 2) (run "(cond (false 0) (() 1) (\"hi\" 2) (false 3))")
+        assertEq (Int 1) (run "(cond (() 0) (true 1) (() 2))")
