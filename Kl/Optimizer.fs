0af415b16348c62f4480ba07cb1846d51f783079
rkoeninger <rkoeninger@att.net>
2017-01-05 00:03:32 -0500

    Added Optimizer

---
+++ Kl/Optimizer.fs
@@ -0,0 +1,12 @@
+﻿namespace Kl
+
+open ExpressionPatterns
+
+module Optimizer =
+
+    let simplify expr =
+        match expr with
+
+        // Eta-reduction on lambda declaration
+        | LambdaExpr(param0, AppExpr(f, [Sym param1])) when param0 = param1 -> f
+        | _ -> expr
