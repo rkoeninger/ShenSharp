7f9393b116ff9ea4f3ef9cbfbfffecf9bd8d6197
rkoeninger <rkoeninger@att.net>
2017-01-24 18:57:06 -0500

    Broke compiler code into multiple functions

--- Kl.Load/Compiler.fs
+++ Kl.Load/Compiler.fs
@@ -69,49 +69,56 @@ module Compiler =
         printfn ""
         globals
 
+    let installerSyntax globals =
+        let buffer = new StringBuilder()
+        let line s = buffer.AppendLine s |> ignore
+        line "namespace Kl.Installation"
+        line "open Kl"
+        line "open Kl.Values"
+        line "type Installer() ="
+        line "    static member Install(globals: Globals) ="
+        let origGlobals = baseGlobals()
+        for kv in globals.Symbols do
+            if not(Seq.contains kv.Key origGlobals.Symbols.Keys) then
+                let arrayRefs = buildRefs (findArrays kv.Value)
+                for (array, arrayName) in arrayRefs do
+                    // TODO: not finding the nested (@v 0) vector while searching the *property-vector*
+                    line <| "        let " + arrayName + " = Array.create " + string (Array.length array) + " (Sym \"shen.fail!\")"
+                    for (index, item) in Seq.zip (seq {0 .. 20001}) array do
+                        if item <> Sym "shen.fail!" then
+                            line <| "        " + arrayName + ".[" + string index + "] <- " + encode arrayRefs item
+                encodeSymbol (fun s -> line <| "        " + s) arrayRefs kv.Key kv.Value
+        for kv in globals.Functions do
+            if not(Seq.contains kv.Key origGlobals.Functions.Keys || List.contains kv.Key ["exit"]) then
+                encodeFunction (fun s -> line <| "        " + s) [] kv.Key kv.Value
+        line "        ()"
+        buffer.ToString()
+
+    let emitDll name =
+        let service = new SimpleSourceCodeServices()
+        let options = [|
+            "fsc.exe"
+            "-o"; name + ".dll"
+            "-a"; name + ".fs"
+            "-r"; "Kl.dll"
+        |]
+        let (errors, returnCode) = service.Compile options
+        if returnCode <> 0 then
+            raise <| new Exception(String.Join(", ", Seq.map (fun (e: FSharpErrorInfo) -> e.Message) errors))
+
     let compile klFolder klFiles =
         if not(File.Exists "Installer.dll") then
             let globals = load klFolder klFiles
             printfn "Generating installation code..."
-            let buffer = new StringBuilder()
-            let line s = buffer.AppendLine s |> ignore
-            line "namespace Kl.Installation"
-            line "open Kl"
-            line "open Kl.Values"
-            line "type Installer() ="
-            line "    static member Install(globals: Globals) ="
-            let origGlobals = baseGlobals()
-            for kv in globals.Symbols do
-                if not(Seq.contains kv.Key origGlobals.Symbols.Keys) then
-                    let arrayRefs = buildRefs (findArrays kv.Value)
-                    for (array, arrayName) in arrayRefs do
-                        // TODO: not finding the nested (@v 0) vector while searching the *property-vector*
-                        line <| "        let " + arrayName + " = Array.create " + string (Array.length array) + " (Sym \"shen.fail!\")"
-                        for (index, item) in Seq.zip (seq {0 .. 20001}) array do
-                            if item <> Sym "shen.fail!" then
-                                line <| "        " + arrayName + ".[" + string index + "] <- " + encode arrayRefs item
-                    encodeSymbol (fun s -> line <| "        " + s) arrayRefs kv.Key kv.Value
-            for kv in globals.Functions do
-                if not(Seq.contains kv.Key origGlobals.Functions.Keys || List.contains kv.Key ["exit"]) then
-                    encodeFunction (fun s -> line <| "        " + s) [] kv.Key kv.Value
-            line "        ()"
-            File.WriteAllText("Installer.fs", buffer.ToString())
+            let syntax = installerSyntax globals
+            File.WriteAllText("Installer.fs", syntax)
             printfn "Compiling installation code..."
-            let service = new SimpleSourceCodeServices()
-            let options = [|
-                "fsc.exe"
-                "-o"; "Installer.dll"
-                "-a"; "Installer.fs"
-                "-r"; "Kl.dll"
-            |]
-            let (errors, returnCode) = service.Compile options
-            if returnCode <> 0 then
-                raise <| new Exception(String.Join(", ", Seq.map (fun (e: FSharpErrorInfo) -> e.Message) errors))
+            emitDll "Installer"
             printfn "Installation code cached."
             printfn ""
             globals
         else
-            printfn "Loading cached assembly..."
+            printfn "Loading cached runtime..."
             printfn ""
             let assembly = Assembly.LoadFile(Path.Combine(Environment.CurrentDirectory, "Installer.dll"))
             let installation = assembly.GetType("Kl.Installation.Installer")
