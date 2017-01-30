8eb5d725a4aa3faab090036794ea6955d12b7b0a
rkoeninger <rkoeninger@att.net>
2017-01-29 19:31:37 -0500

    Improved naming of tail recursion tests

--- Kl.Tests/Trampolines.fs
+++ Kl.Tests/Trampolines.fs
@@ -8,57 +8,57 @@ open Kl.Import.Reader
 open TestCommon
 
 [<TestFixture>]
-type TailRecursionTests() =
+type Trampolines() =
 
     let attempt body =
         assertTrue (sprintf "(defun count-down (X) %s) (count-down 20000)" body)
 
     [<Test>]
-    member this.``trampolines optimize tail recursive calls in an if consequent expression``() =
+    member this.``optimize tail recursive calls in an if consequent expression``() =
         attempt "(if (> X 0) (count-down (- X 1)) true)"
 
     [<Test>]
-    member this.``trampolines optimize tail recursive calls in an if alternative expression``() =
+    member this.``optimize tail recursive calls in an if alternative expression``() =
         attempt "(if (<= X 0) true (count-down (- X 1)))"
 
     [<Test>]
-    member this.``trampolines optimize tail recursive calls in nested if expressions``() =
+    member this.``optimize tail recursive calls in nested if expressions``() =
         attempt "(if (<= X 0) true (if true (count-down (- X 1)) false))"
 
     [<Test>]
-    member this.``trampolines optimize tail recursive calls in a let body``() =
+    member this.``optimize tail recursive calls in a let body``() =
         attempt "(if (<= X 0) true (let F 1 (count-down (- X F))))"
 
     [<Test>]
-    member this.``trampolines optimize tail recursive calls in a first cond consequent``() =
+    member this.``optimize tail recursive calls in a first cond consequent``() =
         attempt "(cond ((> X 0) (count-down (- X 1))) (true true))"
 
     [<Test>]
-    member this.``trampolines optimize tail recursive calls in a last cond consequent``() =
+    member this.``optimize tail recursive calls in a last cond consequent``() =
         attempt "(cond ((<= X 0) true) (true (count-down (- X 1))))"
 
     [<Test>]
-    member this.``trampolines optimize tail recursive calls at the end of a do``() =
+    member this.``optimize tail recursive calls at the end of a do``() =
         attempt "(do 0 (if (<= X 0) true (do 0 (count-down (- X 1)))))"
 
     [<Test>]
-    member this.``trampolines optimize tail recursive calls in handler of trap expression``() =
+    member this.``optimize tail recursive calls in handler of trap expression``() =
         attempt "(trap-error (if (> X 0) (simple-error \"recur\") true) (lambda E (count-down (- X 1))))"
 
     [<Test>]
-    member this.``trampolines optimize through freeze calls``() =
+    member this.``optimize through freeze calls``() =
         attempt "(if (<= X 0) true ((freeze (count-down (- X 1)))))"
 
     [<Test>]
-    member this.``trampolines optimize through lambda calls``() =
+    member this.``optimize through lambda calls``() =
         attempt "(if (<= X 0) true ((lambda Y (count-down (- X Y))) 1))"
 
     [<Test>]
-    member this.``trampolines optimize through recursive lambdas``() =
+    member this.``optimize through recursive lambdas``() =
         assertTrue "(let F (lambda F (lambda X (if (<= X 0) true ((F F) (- X 1))))) ((F F) 20000))"
 
     [<Test>]
-    member this.``trampolines optimize through recursive freezes``() =
+    member this.``optimize through recursive freezes``() =
         assertTrue
             "(set counter 20000)
              (defun decrement () (set counter (- (value counter) 1)))
@@ -66,7 +66,7 @@ type TailRecursionTests() =
              ((value count-down))"
 
     [<Test>]
-    member this.``trampolines optimize through zero-arg defuns``() =
+    member this.``optimize through zero-arg defuns``() =
         assertTrue
             "(set counter 20000)
              (defun decrement () (set counter (- (value counter) 1)))
@@ -74,7 +74,7 @@ type TailRecursionTests() =
              (count-down)"
 
     [<Test>]
-    member this.``trampolines optimize mutually-recursive functions``() =
+    member this.``optimize mutually-recursive functions``() =
         assertFalse
             "(defun odd? (X) (if (= 0 X) false (even? (- X 1))))
              (defun even? (X) (if (= 0 X) true (odd? (- X 1))))
