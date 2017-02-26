4a4b1c1bb94bf721b256154d90c2989d17c2ca3b
rkoeninger <rkoeninger@att.net>
2017-02-26 00:24:55 -0500

    Added running of test suite to CI scripts

--- Shen.Tests/RunTestSuite.fs
+++ Shen.Tests/RunTestSuite.fs
@@ -1,16 +1,23 @@
 ﻿module Shen.Tests
 
+open System
+open System.IO
 open System.Threading
 open Kl
 open Kl.Values
 open Kl.Evaluator
 open Shen.Runtime
 
+let rec combine = function
+    | [] -> "."
+    | [x] -> x
+    | x :: xs -> Path.Combine(x, combine xs)
 let stackSize = 16777216
-let testFolder = @"..\..\..\Distribution\Tests"
+let testFolder = combine [".."; ".."; ".."; "Distribution"; "Tests"]
 
 let runTestSuite () =
     let globals = newRuntime()
+    define globals "y-or-n?" (Compiled(0, fun _ _ -> Environment.Exit 1; Empty))
     eval globals (toCons [Sym "cd"; Str testFolder]) |> ignore
     eval globals (toCons [Sym "load"; Str "README.shen"]) |> ignore
     eval globals (toCons [Sym "load"; Str "tests.shen"]) |> ignore
