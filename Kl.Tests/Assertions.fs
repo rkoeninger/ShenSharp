bc05ebaf882a2bed989ee49d30686b314bd01dae
rkoeninger <rkoeninger@att.net>
2017-02-26 21:48:48 -0500

    Added Shen.Tests and updated CI scripts
    Made Kl.Tests a module

--- Kl.Tests/Assertions.fs
+++ Kl.Tests/Assertions.fs
@@ -1,4 +1,4 @@
-﻿namespace Kl.Tests
+﻿module Kl.Tests.Assertions
 
 open NUnit.Framework
 open Kl
@@ -7,26 +7,24 @@ open Kl.Extensions
 open Kl.Evaluator
 open Kl.Startup
 open Kl.Make.Reader
-
-module Assertions =
     
-    let runIn globals syntax = eval globals (read syntax)
-    let run = runIn (baseGlobals())
-    let runAll syntax =
-        let globals = baseGlobals()
-        List.fold (fun _ -> eval globals) Empty (readAll syntax)
-    let assertEq (expected: 'a) (actual: 'a) = Assert.AreEqual(expected, actual)
-    let assertNotEq (expected: 'a) (actual: 'a) = Assert.AreNotEqual(expected, actual)
-    let assertTrue syntax = assertEq True (runAll syntax)
-    let assertFalse syntax = assertEq False (runAll syntax)
-    let assertEach pairs =
-        let globals = baseGlobals()
-        List.iter (fun (expected, syntax) -> assertEq expected (runIn globals syntax)) pairs
-    let assertNoError syntax = runAll syntax |> ignore
-    let assertError syntax =
-        try
-            runAll syntax |> ignore
-            Assert.Fail "Error expected"
-        with
-            | :? AssertionException as e -> raise e
-            | _ -> ()
+let runIn globals syntax = eval globals (read syntax)
+let run = runIn (baseGlobals())
+let runAll syntax =
+    let globals = baseGlobals()
+    List.fold (fun _ -> eval globals) Empty (readAll syntax)
+let assertEq (expected: 'a) (actual: 'a) = Assert.AreEqual(expected, actual)
+let assertNotEq (expected: 'a) (actual: 'a) = Assert.AreNotEqual(expected, actual)
+let assertTrue syntax = assertEq True (runAll syntax)
+let assertFalse syntax = assertEq False (runAll syntax)
+let assertEach pairs =
+    let globals = baseGlobals()
+    List.iter (fun (expected, syntax) -> assertEq expected (runIn globals syntax)) pairs
+let assertNoError syntax = runAll syntax |> ignore
+let assertError syntax =
+    try
+        runAll syntax |> ignore
+        Assert.Fail "Error expected"
+    with
+        | :? AssertionException as e -> raise e
+        | _ -> ()
---
+++ Shen.Tests/Assertions.fs
@@ -0,0 +1,14 @@
+﻿module Shen.Tests.Assertions
+
+open NUnit.Framework
+open Kl
+open Kl.Values
+open Shen.Runtime
+
+let assertTrue syntax =
+    let globals = newRuntime()
+    Assert.IsTrue((True = Eval(globals, syntax)))
+
+let assertFalse syntax =
+    let globals = newRuntime()
+    Assert.IsTrue((False = Eval(globals, syntax)))
