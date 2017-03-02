adc106311895a0a0a0b87f8e3b20b58bb3c00def
Robert Koeninger <rkoeninger@att.net>
2017-03-02 17:47:44 -0500

    Added start of Shen.Pack

--- Shared.fs
+++ Shared.fs
@@ -4,15 +4,24 @@ open System.Reflection
 open System.Runtime.InteropServices
 
 [<Literal>]
+let Product = "ShenSharp"
+
+[<Literal>]
+let Description = "Shen for the Common Language Runtime"
+
+[<Literal>]
 let Author = "Robert Koeninger"
+
+[<Literal>]
+let Copyright = "Copyright © 2015-2017 " + Author
 
 [<Literal>]
 let Revision = "0.7.0.0"
 
-[<assembly: AssemblyProduct "ShenSharp">]
-[<assembly: AssemblyDescription "Shen for the Common Language Runtime">]
+[<assembly: AssemblyProduct(Product)>]
+[<assembly: AssemblyDescription(Description)>]
 [<assembly: AssemblyCompany(Author)>]
-[<assembly: AssemblyCopyright("Copyright © 2015-2017 " + Author)>]
+[<assembly: AssemblyCopyright(Copyright)>]
 [<assembly: AssemblyVersion(Revision)>]
 [<assembly: AssemblyFileVersion(Revision)>]
 [<assembly: AssemblyInformationalVersion(Revision)>]
---
+++ Shen.Pack/BuildPackage.fs
@@ -0,0 +1,42 @@
+﻿module Shen.BuildPackage
+
+open System
+open System.IO
+open NuGet
+open ShenSharp.Shared
+
+let addFile (builder: PackageBuilder) source target =
+    let file = new PhysicalPackageFile()
+    file.SourcePath <- source
+    file.TargetPath <- target
+    builder.Files.Add file
+
+[<EntryPoint>]
+let main args =
+
+    // TODO: do ILRepack, like in package.{bat | sh}
+
+    let builder = new PackageBuilder()
+    builder.Id <- Product
+    builder.Title <- Product
+    builder.Description <- Description
+    builder.Summary <- Description // TODO: pull from readme.md
+    builder.Authors.Add Author |> ignore
+    builder.Owners.Add Author |> ignore
+    builder.Copyright <- Copyright
+    builder.Version <- SemanticVersion.Parse Revision
+    // TODO: builder.LicenseUrl // TODO: pull from license.txt
+    builder.RequireLicenseAcceptance <- false
+    builder.Tags.Add "Shen" |> ignore // TODO: pull from ?
+    //builder.ReleaseNotes // TODO: pull from changelog.md
+    builder.ProjectUrl <- new Uri("https://github.com/rkoeninger/ShenSharp") // TODO: pull from .git ?
+    // TODO: builder.MinClientVersion
+    // TODO: use Values.combine and distinguish Debug/Release
+    // TODO: select everything under Package/ folder
+    addFile builder @"..\..\..\Artifacts\Debug\Package\lib\Shen.dll" @"lib\net40\Shen.dll"
+    addFile builder @"..\..\..\Artifacts\Debug\Package\tools\Shen.exe" @"tools\Shen.exe"
+    // TODO: move relative paths into ShenSharp.Shared?
+    let packageFileName = sprintf "%s.%s.nupkg" Product Revision
+    // builder.Save(File.Open(@"..\..\..\Artifacts\Debug\" + packageFileName, FileMode.OpenOrCreate, FileAccess.Write))
+    // TODO: ^ raises IO exception
+    0
