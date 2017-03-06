40cb59d7596b4d5a2c39675dd006bfe0823eab82
Robert Koeninger <rkoeninger@att.net>
2017-03-06 13:49:46 -0500

    Expanded help output for Shen.Repl

--- Shen.Repl/RunRepl.fs
+++ Shen.Repl/RunRepl.fs
@@ -6,16 +6,27 @@ open Kl
 open Kl.Values
 open Kl.Evaluator
 open Shen.Runtime
+open ShenSharp.Shared
 
 let runRepl files () =
     let globals = newRuntime()
     try
         match files with
-        | ["-h"] ->
-            printfn "shen            : Starts REPL"
-            printfn "shen -e <expr>  : Evaluates expr and prints result"
-            printfn "shen <file>*    : Loads files in given order"
-        | "-e" :: rest ->
+        | ("--help" | "-h") :: _ ->
+            let shenVersion =
+                match retrieve globals "*version*" with
+                | Str s -> s
+                | _ -> "Unknown"
+            printfn "%s" shenVersion
+            printfn "ShenSharp %s" Revision
+            printfn ""
+            printfn "Usage: shen [MODE] [FILES]..."
+            printfn "  -h, --help                  : Shows this help"
+            printfn "  -e, --eval <expr>           : Evaluates expr and prints result"
+            printfn ""
+            printfn "Loads [FILES] in order"
+            printfn "Starts the REPL if no mode/files specified"
+        | ("--eval" | "-e") :: rest ->
             printfn "%O" (evalSyntax globals (String.Join(" ", rest)))
         | [] ->
             eval globals (toCons [Sym "shen"]) |> ignore
