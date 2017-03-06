fc0b0a163d7367a255fdb1293acf0e9a7922118a
Robert Koeninger <rkoeninger@att.net>
2017-03-06 15:38:10 -0500

    Added doc comments to functions/methods in Shen.Api

--- Shen.Api/ExtensionMethods.fs
+++ Shen.Api/ExtensionMethods.fs
@@ -5,37 +5,57 @@ open Kl
 open Shen.Language
 open Shen.Runtime
 
+/// <summary>
+/// Extension methods to make working with Shen environment
+/// look more similiar to a conventional C# API.
+/// </summary>
 [<Extension>]
 type ExtensionMethods() =
 
-// TODO: add doc comments
-
+    /// <summary>
+    /// Evaluates given Shen syntax and returns result as KL Value.
+    /// </summary>
     [<Extension>]
     static member Eval(globals: Globals, syntax: string) =
         evalSyntax globals syntax
 
+    /// <summary>
+    /// Loads Shen file at given path.
+    /// </summary>
     [<Extension>]
     static member Load(globals: Globals, path: string) =
         load globals path
 
+    /// <summary>
+    /// Defines a zero-parameter native function.
+    /// </summary>
     [<Extension>]
     static member DefineFunction(globals: Globals,
                                  name: string,
                                  native: Globals -> Value) =
         defineFunction0 globals name native
 
+    /// <summary>
+    /// Defines a one-paramter native function.
+    /// </summary>
     [<Extension>]
     static member DefineFunction(globals: Globals,
                                  name: string,
                                  native: Globals -> Value -> Value) =
         defineFunction1 globals name native
 
+    /// <summary>
+    /// Defines a two-paramter native function.
+    /// </summary>
     [<Extension>]
     static member DefineFunction(globals: Globals,
                                  name: string,
                                  native: Globals -> Value -> Value -> Value) =
         defineFunction2 globals name native
 
+    /// <summary>
+    /// Defines a Shen macro using a native function.
+    /// </summary>
     [<Extension>]
     static member DefineMacro(globals: Globals, name: string, native: Value -> Value) =
         defineMacro globals name native
--- Shen.Api/Runtime.fs
+++ Shen.Api/Runtime.fs
@@ -5,36 +5,57 @@ open Kl.Values
 open Kl.Startup
 open Shen.Language
 
+/// <summary>
+/// Creates a new Shen runtime environment.
+/// </summary>
 [<CompiledName "NewRuntime">]
 let newRuntime() = install(baseGlobals())
 
+/// <summary>
+/// Evaluates given Shen syntax and returns result as KL Value.
+/// </summary>
 [<CompiledName "EvalSyntax">]
 let evalSyntax globals syntax = kl_eval globals [kl_read globals [pipeString syntax]]
 
+/// <summary>
+/// Loads Shen file at given path.
+/// </summary>
 [<CompiledName "Load">]
 let load globals path = kl_load globals [Str path] |> ignore
 
+/// <summary>
+/// Defines a zero-paramter native function.
+/// </summary>
 [<CompiledName "DefineFunction0">]
 let defineFunction0 globals name native =
     let f globals = function
         | [] -> native globals
         | args -> argsErr name ["value"] args
     define globals name (Compiled(0, f))
-
+    
+/// <summary>
+/// Defines a one-paramter native function.
+/// </summary>
 [<CompiledName "DefineFunction1">]
 let defineFunction1 globals name native =
     let f globals = function
         | [x] -> native globals x
         | args -> argsErr name ["value"] args
     define globals name (Compiled(1, f))
-
+    
+/// <summary>
+/// Defines a two-paramter native function.
+/// </summary>
 [<CompiledName "DefineFunction1">]
 let defineFunction2 globals name native =
     let f globals = function
         | [x; y] -> native globals x y
         | args -> argsErr name ["value"] args
     define globals name (Compiled(2, f))
-
+    
+/// <summary>
+/// Defines a Shen macro using a native function.
+/// </summary>
 [<CompiledName "DefineMacro">]
 let defineMacro globals name native =
     let f _ = function
