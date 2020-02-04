3cfc1b23c4c16dde757982e0ebbd212dc19287d5
Robert Koeninger <rkoeninger@att.net>
2020-02-04 17:24:25 -0500

    Fixed test suite run

--- Shen.TestSuite/RunTestSuite.fs
+++ Shen.TestSuite/RunTestSuite.fs
@@ -3,18 +3,18 @@
 open System
 open Kl
 open Kl.Values
-open Kl.Evaluator
 open Shen.Runtime
 open ShenSharp.Shared
 
 let testFolder = fromRoot ["packages"; KernelFolderName; "tests"]
 
 let runTestSuite () =
-    let globals = newRuntime()
+    let globals = newRuntime ()
     define globals "y-or-n?" (Compiled(0, fun _ _ -> Environment.Exit 1; Empty))
-    eval globals (toCons [Sym "cd"; Str testFolder]) |> ignore
-    eval globals (toCons [Sym "load"; Str "README.shen"]) |> ignore
-    eval globals (toCons [Sym "load"; Str "tests.shen"]) |> ignore
+    Environment.CurrentDirectory <- testFolder
+    assign globals "*home-directory*" <| Str testFolder
+    load globals "README.shen" |> ignore
+    load globals "tests.shen" |> ignore
 
 [<EntryPoint>]
 let main _ = separateThread16MB runTestSuite
