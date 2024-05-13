a6810cb3530c8bf048cd04ca2ff072f232f39a49
Jacob MacDonald <jaccarmac@gmail.com>
2024-05-12 21:25:24 -0500

    Revert "Remove call to initialise."
    This reverts commit 1724429e311f721930883d6f2bbb929d59929685.

--- src/Kl.Make/Compiler.fs
+++ src/Kl.Make/Compiler.fs
@@ -235,6 +235,7 @@ let buildInstallationFile name exprs =
                     (List.concat
                         [
                             List.map installDefun exprs
+                            [appExprN (idExpr "kl_shen.initialise") [(idExpr "globals"); (listExpr [])] |> appIgnore]
                             [idExpr "globals"]
                         ]))
         ]
